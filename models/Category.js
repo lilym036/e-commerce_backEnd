a0efe2c0862d9736ad7bc333e1cd528683ab31db
Lilibeth Mejia <mejialily036@gmail.com>
2023-07-30 19:15:19 -0500

    added database models code

--- models/Category.js
+++ models/Category.js
@@ -2,15 +2,26 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Category extends Model {}
+class Category extends Model { }
 
 Category.init(
   {
     // define columns
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
   },
   {
     sequelize,
     timestamps: false,
+    // Prevent sequelize from renaming the table
     freezeTableName: true,
     underscored: true,
     modelName: 'category',
